26919a9765f0d1ca2ec34a8e2a978106f50e4efd
Thomas <thomas.gourouza@gmail.com>
2022-10-16 17:50:50 +0200

    implement sub tab

--- src/app/components/tabs/home/home.component.ts
+++ src/app/components/tabs/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { TranslateService } from "@ngx-translate/core";
+// import { TranslateService } from "@ngx-translate/core";
 import { NavigationService } from 'src/app/services/navigation.service';
 
 @Component({
@@ -11,20 +11,23 @@ import { NavigationService } from 'src/app/services/navigation.service';
 export class HomeComponent implements OnInit {
 
   public name = 'Thomas';
+  public showIntro: boolean;
 
   constructor(
-    private translate: TranslateService,
+    // private translate: TranslateService,
     private navigationService: NavigationService,
     private router: Router
-  ) { }
+  ) {
+    this.showIntro = this.router.url.includes('introduction');
+    this.navigationService.setActiveTabs(this.router.url);
+  }
 
   ngOnInit(): void {
-    this.navigationService.setActiveTabs(this.router.url);
     
-    console.log(this.translate.instant('demo.greeting', { name: 'Bob' }));
-    this.translate.get('demo.greeting', { name: 'John' }).subscribe((res: string) => {
-      console.log(res);
-    });
+    // console.log(this.translate.instant('demo.greeting', { name: 'Bob' }));
+    // this.translate.get('demo.greeting', { name: 'John' }).subscribe((res: string) => {
+    //   console.log(res);
+    // });
   }
 
 }
